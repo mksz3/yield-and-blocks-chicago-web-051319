149dd279b0f3d21984a2e1deb07af021afd4b3cf
Markus Sanders <markus.sanderst@gmail.com>
2019-05-01 19:03:57 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,14 @@
 def hello_t(array)
-  array.each { |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
-  }
-  puts "Hey! No block was given!"
+  if array {
+    array.each { |name|
+      if name.start_with?("T")
+        puts "Hi, #{name}"
+      end
+    }
+    array
+  else 
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
