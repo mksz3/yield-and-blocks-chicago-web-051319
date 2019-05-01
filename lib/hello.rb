9c8dc7c0c93b74a4ea0eba69d137731072b6a7b8
Markus Sanders <markus.sanderst@gmail.com>
2019-05-01 19:00:15 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,16 +1,7 @@
 def hello_t(array)
-  i = 0
-  while i < array.length
-    yield array[i]
-    i+= 11
-  end
-  array
+  
 end
 
 # call your method here!
 
-hello_t(array) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+hello_t(array)
